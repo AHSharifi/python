6b9fb87dd1e25e511943771a16cf8b71e036ad88
ahsharifi <mr.amirhossein88@gmail.com>
2026-07-18 16:23:42 -0400

    Add: Inputs that get info from user and save it in variables and show them.

---
+++ S2/session2.py
@@ -0,0 +1,79 @@
+# [========= Output =========]
+# print("Hello World!")
+
+# [========= Variables =========]
+name = "Amir"
+age = 18
+group = 'c'
+isActive = True
+
+# print(name)
+# print(age)
+# print(group)
+# print(isActive)
+
+# [========= Type =========]
+
+# print(type(name))
+# print(type(age))
+# print(type(group))
+# print(type(isActive))
+
+# [========= Input =========]
+
+# name = input("Enter your name: ")
+
+# print(name)
+# print(type(name))
+
+# [========= Operators =========]
+
+# num1 = 4
+# num2 = 2
+
+# + : jam
+# print(num1 + num2)
+
+# - : tafrigh
+# print(num1 - num2)
+
+# / : taghsim
+# print(num1 / num2)
+
+# * : zarb
+# print(num1 * num2)
+
+# // : kharej ghesmat
+# print(num1 // num2)
+
+# % : baghi mande
+# print(num1 % num2)
+
+# ** : tavan
+# print(num1 ** num2)
+
+# < , > : bozorghtar & kuchektar
+# print(num1 > num2)
+# print(num1 < num2)
+
+# == : mosavi
+# print(num1 == num2)
+
+# != : mosavi nabashad
+# print(num1 != num2)
+
+# and : &&
+# print(num1 < 10 and num2 > 5)
+
+# or : ||
+# print(num1 < 10 or num2 > 5)
+
+# not : !
+# print(not(11>12))
+
+# [========= Conditions =========]
+
+# if (isActive):
+#   print("user is active.")
+# else:
+#   print('user is not active.')
