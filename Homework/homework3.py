fc6b19f91d80c398b61dc04d121609c6eb05f1b8
ahsharifi <mr.amirhossein88@gmail.com>
2026-07-22 15:22:48 -0400

    Add: append new food to list and remove third food from the list

--- Homework/homework3.py
+++ Homework/homework3.py
@@ -1,3 +1,11 @@
 # make foods list
 foods_list = ["kebab", "chicken", "pizza", "sandwich", "cheese burgur"]
 print(foods_list)
+
+# add new food to the list
+foods_list.append("pasta")
+print(foods_list)
+
+# remove 3rd food of list
+foods_list.remove(foods_list[2])
+print(foods_list)
