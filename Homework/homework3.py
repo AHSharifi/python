167387921c31f2ebef0927f2c0f78cfc3e285b0e
ahsharifi <mr.amirhossein88@gmail.com>
2026-07-22 15:42:07 -0400

    Add: factorial formula with for loop.

--- Homework/homework3.py
+++ Homework/homework3.py
@@ -1,11 +1,23 @@
-# make foods list
-foods_list = ["kebab", "chicken", "pizza", "sandwich", "cheese burgur"]
-print(foods_list)
+# # make foods list
+# foods_list = ["kebab", "chicken", "pizza", "sandwich", "cheese burgur"]
+# print(foods_list)
 
-# add new food to the list
-foods_list.append("pasta")
-print(foods_list)
+# # add new food to the list
+# foods_list.append("pasta")
+# print(foods_list)
 
-# remove 3rd food of list
-foods_list.remove(foods_list[2])
-print(foods_list)
+# # remove 3rd food of list
+# foods_list.remove(foods_list[2])
+# print(foods_list)
+
+# print("====================================================")
+
+# [================ Factorial ================]
+
+number = int(input("Enter your number: "))
+factorial = number
+
+for i in range(1, number):
+  factorial *= (number - i)
+
+print(factorial)
